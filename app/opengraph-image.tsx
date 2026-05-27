d3d8a918be54c480428b288aee70f2de10f004cd
nanhyunsoo <wimpy0410@gmail.com>
2026-05-27 16:12:47 +0900

    feat: OG 이미지 디자인 고도화 및 인스타그램 호환성 강화

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -2,7 +2,7 @@ import { ImageResponse } from "next/og";
 
 export const runtime = "nodejs";
 
-export const alt = "MyLink";
+export const alt = "MyLink - Development in One Link";
 export const size = {
   width: 1200,
   height: 630,
@@ -15,19 +15,85 @@ export default function Image() {
     (
       <div
         style={{
-          backgroundColor: "black",
+          background: "#0D0D0D",
           width: "100%",
           height: "100%",
           display: "flex",
+          flexDirection: "column",
           alignItems: "center",
           justifyContent: "center",
-          color: "white",
-          border: "20px solid white",
+          border: "24px solid white",
+          padding: "40px",
         }}
       >
-        <h1 style={{ fontSize: "128px", fontWeight: "900", fontStyle: "italic" }}>
-          MyLink
-        </h1>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            border: "8px solid white",
+            padding: "60px 80px",
+            backgroundColor: "black",
+            position: "relative",
+          }}
+        >
+          {/* Offset Shadow effect */}
+          <div 
+            style={{
+              position: "absolute",
+              top: "20px",
+              left: "20px",
+              right: "-20px",
+              bottom: "-20px",
+              backgroundColor: "rgba(255, 255, 255, 0.15)",
+              zIndex: -1,
+            }}
+          />
+          
+          <h1
+            style={{
+              fontSize: "120px",
+              fontWeight: 900,
+              color: "white",
+              margin: 0,
+              lineHeight: 1,
+              letterSpacing: "-0.05em",
+              textTransform: "uppercase",
+              fontStyle: "italic",
+            }}
+          >
+            MyLink
+          </h1>
+          <p
+            style={{
+              fontSize: "32px",
+              fontWeight: 700,
+              color: "rgba(255, 255, 255, 0.7)",
+              margin: "24px 0 0 0",
+              letterSpacing: "0.3em",
+              textTransform: "uppercase",
+            }}
+          >
+            Development in One Link
+          </p>
+        </div>
+        
+        <div 
+          style={{
+            position: "absolute",
+            bottom: "60px",
+            display: "flex",
+            alignItems: "center",
+            gap: "10px",
+          }}
+        >
+          <div style={{ width: "40px", height: "2px", backgroundColor: "white" }} />
+          <span style={{ color: "white", fontSize: "16px", fontWeight: 900, letterSpacing: "0.4em", textTransform: "uppercase" }}>
+            The Developer's Link Page
+          </span>
+          <div style={{ width: "40px", height: "2px", backgroundColor: "white" }} />
+        </div>
       </div>
     ),
     {
